fdccd0dbeab5d67fade58ae33ad4c141e863030a
Mukesh Pilane <mukeshpilane@gmail.com>
2023-10-08 13:28:35 +0530

    deployement

--- model_api/utils/ranker.py
+++ model_api/utils/ranker.py
@@ -3,8 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.tokenize import word_tokenize
-import nltk
-nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 
 
