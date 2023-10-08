839ebb7d5ccf19c608cede037f3e3c294b4e93fc
Kamlesh0007 <wwwkamleshyadav732@gmail.com>
2023-10-09 00:35:39 +0530

    pdf added

---
+++ src/app/(user)/applications/[id]/components/PdfViewer.tsx
@@ -0,0 +1,29 @@
+
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Required for styles
+
+// Set up worker to handle PDF rendering
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+const PDFViewer = ( props ) => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+  
+    const onDocumentLoadSuccess = ({ numPages }) => {
+      setNumPages(numPages);
+    };
+  
+    return (
+      <div>
+        <Document file={props.base64String} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+    );
+  };
+  
+
+export default PDFViewer;
