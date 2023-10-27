7a68728f08fcebb36b2589a1450dbecac78e16ef
Mukesh Pilane <mukeshpilane@gmail.com>
2023-10-27 22:56:17 +0530

    modal bug

--- src/app/(user)/profile/components/StatusModal.tsx
+++ src/app/(user)/profile/components/StatusModal.tsx
@@ -1,22 +1,38 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { AiOutlineClose } from 'react-icons/ai';
 import { fetchRecommendation } from '~/utils/flaskApi';
 
 
-const StatusModal = async ({ keyword,setIsmodal, ismodal}) => {
-  let Recommendations
-  if(ismodal) {
-   Recommendations = ismodal && await fetchRecommendation(keyword);
-  }
-  console.log(Recommendations);
+interface CourseRecommendation {
+  title: string;
+  organization: string;
+}
+
+const StatusModal = ({ keyword,setIsmodal, ismodal}) => {
+  const [recommendations, setRecommendations] = useState<CourseRecommendation | null>(null);
+
+  useEffect(() => {
+    if (ismodal) {
+      const fetchRecommendations = async () => {
+        try {
+          const data = await fetchRecommendation(keyword);
+          setRecommendations(data);
+        } catch (error) {
+          // Handle any fetch errors here
+          console.error(error);
+        }
+      };
+      
+      fetchRecommendations();
+    }
+  }, [ismodal, keyword]);
+  
   
   return (
     <>
- 
       <AnimatePresence>
-
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -24,6 +40,7 @@ const StatusModal = async ({ keyword,setIsmodal, ismodal}) => {
             transition={{ duration: 0.3 }}
             className="fixed inset-0 flex items-center justify-center z-50"
           >
+            
             <div className="bg-[rgba(0,0,0,0.5)] fixed z-40 inset-0"></div>
             <div className=" bg-white max-w-xl mx-auto max-h-[500px] rounded shadow-lg z-50 overflow-y-auto">
               <button onClick={setIsmodal} className="absolute cursor-pointer px-4 py-2 text-white rounded-lg right-2.5 top-2.5 hover:scale-[1.045] bg-indigo-600 hover:bg-indigo-400">
